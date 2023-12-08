9a552ddef9781818cc6e0326b8ae5be98d6a3da2
yemliha81 <yemlihaddln@hotmail.com>
2023-12-08 12:39:04 +0300

    page image added

--- application/views/page_view.php
+++ application/views/page_view.php
@@ -1,11 +1,15 @@
 <?php include('includes/header.php');?>
 	<div class="holder">
 	    <div class="container">
-		<div class="page-title text-center" style="margin-top:20px; font-size:20px;">
-			<b><?php echo  $page['page_name_en'];?></b>
-		</div>
-		<div style="margin-top:20px;">
-			<?php echo  $page['page_description_en'];?>
+			<div>
+				<img src="<?php echo $_ENV['BASE_URL'];?>admin/files/page/img/1000/<?php echo  $page['page_image'];?>" width="100%">
+			</div>
+			<div class="page-title text-center" style="margin-top:20px; font-size:20px;">
+				<b><?php echo  $page['page_name_en'];?></b>
+			</div>
+			<div style="margin-top:20px;">
+				<?php echo  $page['page_description_en'];?>
+			</div>
         </div>
-</div>
+	</div>
 <?php include('includes/footer.php');?>
